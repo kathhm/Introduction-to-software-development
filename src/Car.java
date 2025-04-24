e0bf7b5b1f8b677e67e819d74013144a6008330b
Katherine Hinojosa <geofisikatt@gmail.com>
2025-04-23 18:46:53 -0600

    creacion clase Car y clase Main

---
+++ src/Car.java
@@ -0,0 +1,10 @@
+public class Car {
+    String brand;
+    String model;
+    int year;
+    String color;
+
+    public String getCarDetails(){
+        return "the car is brand: " + brand + ", model: " + model + ", year: " + year + " and color: " + color;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main (String[] args){
+        Car myFirstCar = new Car();
+        myFirstCar.model = "GT600";
+        myFirstCar.brand = "Suzuki";
+        myFirstCar.color = "white";
+        myFirstCar.year = 2023;
+
+        System.out.println(myFirstCar.getCarDetails());
+
+        Car[] cars = new Car[5];
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; i < cars.length; i++){
+            cars[i] = new Car();
+
+            System.out.println("Insert car details, for car number: " + (i+1));
+
+        System.out.print("Insert make: ");
+        cars[i].brand = scanner.nextLine();
+
+        System.out.print("Insert model: ");
+        cars[i].model = scanner.nextLine();
+
+        System.out.print("Insert color: ");
+        cars[i].color = scanner.nextLine();
+
+        System.out.print("Insert year: ");
+        cars[i].year = scanner.nextInt();
+        scanner.nextLine();
+
+        }
+        System.out.println("List of cars: ");
+        for (Car car : cars){
+            System.out.println(car.getCarDetails());
+        }
+
+        scanner.close();
+
+
+
+    }
+
+    }
